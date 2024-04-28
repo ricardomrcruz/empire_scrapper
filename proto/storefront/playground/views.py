147fd4051bf36ac2068214034e8bc75b8840b718
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-04-28 03:25:05 +0200

    item card

--- proto/storefront/playground/views.py
+++ proto/storefront/playground/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from item.models import Category, Item
 
 
 def index(request):
-    return render(request, "playground/index.html")
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Item.objects.all()
+
+    return render(request, "playground/index.html", {
+        'categories': categories,
+        'items': items, 
+    })
 
 
 def contact(request):
